f8c4881cd841f7f6d412e26e79f69a4b80e1f02a
Edxon Meneses <edxon.meneses@gmail.com>
2021-11-22 14:26:07 -0500

    Last touches

--- src/components/Users/User/User.js
+++ src/components/Users/User/User.js
@@ -3,7 +3,10 @@ import React from "react";
 const User = ({ user, deleteUser }) => {
   return (
     <div>
-      <div className="card mt-3 container" onClick={() => deleteUser(user.id)}>
+      <div
+        className="card mt-3 container item"
+        onClick={() => deleteUser(user.id)}
+      >
         <div className="card-body">
           <div className="container">
             <p className="form-control">
